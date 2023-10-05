2a681034236f309a83136c75432106d5287c0893
Johannes Grafen <johannes.grafen@rwth-aachen.de>
2023-10-05 16:33:29 +0200

    loss functions

--- labs/ex02/template/costs.py
+++ labs/ex02/template/costs.py
@@ -4,8 +4,8 @@
 import numpy as np
 
 
-def compute_loss(y, tx, w):
-    """Calculate the loss using either MSE or MAE.
+def compute_loss_MSE(y, tx, w):
+    """Calculate the loss MSE
 
     Args:
         y: shape=(N, )
@@ -15,8 +15,21 @@ def compute_loss(y, tx, w):
     Returns:
         the value of the loss (a scalar), corresponding to the input parameters w.
     """
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute loss by MSE
-    # ***************************************************
-    raise NotImplementedError
+    N = y.shape[0]
+    e = y - tx @ w
+    return 1/(2*N) * np.sum(np.square(e))
+    
+def compute_loss_MAE(y, tx, w):
+    """Calculate the loss MAE
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+    N = y.shape[0]
+    e = y - tx @ w
+    return 1/N * np.sum(np.abs(e))
