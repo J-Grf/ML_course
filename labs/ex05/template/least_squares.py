30f2e9b5d6f6da42c13ca14f0933636be7c11ee0
Johannes Grafen <johannes.grafen@rwth-aachen.de>
2023-10-20 22:26:31 +0200

    finished Lab 5

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -5,13 +5,23 @@ Least Square
 """
 
 import numpy as np
-
+import costs
 
 def least_squares(y, tx):
-    """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    """Calculate the least squares solution.
+       returns mse, and optimal weights.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar.
+
+    >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
+    (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
+    """
+    w_star = np.linalg.solve(np.transpose(tx) @ tx, np.transpose(tx) @ y)
+    MSE = costs.compute_mse(y,tx,w_star)
+    return w_star, MSE
